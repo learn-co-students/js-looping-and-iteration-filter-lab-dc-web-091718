5b461cd3cb98ae44d1ad0315e352d6df7683b612
serpentz <absalomcherinet@gmail.com>
2018-10-28 17:21:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file
+function findMatching(collection , elementToMatch){
+return collection.filter(function (element) { return element.toUpperCase() == elementToMatch.toUpperCase(); });
+}
+function fuzzyMatch(collection , partialSearch){
+return collection.filter(function (element) { return element.slice(0, partialSearch.length) == partialSearch});
+}
+function matchName(collection, nameToMatch){
+	return collection.filter(function (element) { return element.name.toUpperCase() == nameToMatch.toUpperCase();});
+}
