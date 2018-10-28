827a3289030e811f496e9e5f565df6e5e9fa818e
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-10-28 13:53:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+	return drivers.filter(function(driverName) {
+		if (driverName.toLowerCase() === name.toLowerCase()) {
+			return driverName;
+		}
+	})
+}
+
+function fuzzyMatch (drivers, pattern) {
+	return drivers.filter(function (driverName) {
+		if (driverName.toLowerCase().startsWith(pattern.toLowerCase())) {
+			return driverName;
+		}
+	})
+}
+
+function matchName (drivers, name) {
+	return drivers.filter(function (driver) {
+		return driver.name.toLowerCase() == name.toLowerCase();
+	})
+}
