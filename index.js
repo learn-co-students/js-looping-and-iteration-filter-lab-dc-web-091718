0161fc13ed31042e3b8a41fe03a71b2444fa0323
nachoespino53 <nachoespino53@gmail.com>
2018-10-28 16:30:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+const findMatching = (obj, item) => {
+  return obj.filter(function (driver) {return driver.toLowerCase() === item.toLowerCase() })
+}
+
+const fuzzyMatch = (obj, search) => {
+  return obj.filter(function (driver) {return driver.slice(0, search.length).toLowerCase() === search.toLowerCase()})
+}
+
+const matchName = (obj, search) => {
+  return obj.filter(function (element) {return element.name.toLowerCase() === search.toLowerCase()});
+}
