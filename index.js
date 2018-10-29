b0607f08f31e985cd00e6583c9e44b8d883799f7
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-10-29 11:15:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, name1) {
+  return drivers.filter(function (name2) {
+    return name2.toLowerCase() === name1.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, namepart) {
+  return drivers.filter(function (name2) {
+    return name2.slice(0, namepart.length).toLowerCase() === namepart.toLowerCase();
+  });
+}
+
+
+function matchName(drivers, name1) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name1.toLowerCase();
+  });
+}
