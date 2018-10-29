820d1c01bb5bb4e49dbf9c519822235cc51f9eed
marycodes2 <mbeliveau92@gmail.com>
2018-10-28 23:48:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, string){
+  return array.filter (function(name) {
+    return name.toUpperCase() === string.toUpperCase()
+  }
+ )
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter (function(name) {
+    return name.slice(0, letters.length) === letters
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter (function(elem){
+    return (elem.name.toUpperCase() === string.toUpperCase())
+  })
+}
